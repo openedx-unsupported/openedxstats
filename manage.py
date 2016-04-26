c0d4dcdba6927b89a3c38bea1f866d8a15c70d14
Joel Barciauskas <joel@edx.org>
2016-04-26 18:38:25 -0400

    Move .env.example, remove dotenv bits from manage.py

--- manage.py
+++ manage.py
@@ -5,9 +5,6 @@ import sys
 import dotenv
 
 if __name__ == "__main__":
-    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-    dotenv.load_dotenv(dotenv_path)
-
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "slack-stats.settings")
 
     from django.core.management import execute_from_command_line
