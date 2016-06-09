5ed938a038efa961b6761711198da45369e4b2f1
Zachary Robbins <zrrobbins@wpi.edu>
2016-06-09 15:00:00 -0400

    Started tests file. Can't continue until script works with entire sites object.

--- openedxstats/apps/sites/tests.py
+++ openedxstats/apps/sites/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from management.commands.import_lang_geo import import_data
 
-# Create your tests here.
+CSV_FILE = '~/Documents/postgres_data/edx_lang_geo_csv.csv'
+
+class ImportScriptTestCase(TestCase):
+
+    def test_import_languages_and_geozones_from_file(self):
+        with open(CSV_FILE, 'rwb') as csvfile:
+            import_data(csvfile) # Import data
+
