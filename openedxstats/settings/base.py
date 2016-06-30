ea2542f6fdc5bd02c8f5fcd0b95682a00caf6ad7
Zachary Robbins <zrrobbins@wpi.edu>
2016-06-30 11:32:40 -0400

    Fixed bug where numbers were sorted alphabetically in DataTable.

--- openedxstats/settings/base.py
+++ openedxstats/settings/base.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 
     'bootstrap3',
@@ -47,6 +48,7 @@ INSTALLED_APPS += PROJECT_APPS
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -101,7 +103,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #    root('assets'),
 #)
 
-# Whitenoise config
+# Whitenoise compression config
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 TEMPLATES = [
