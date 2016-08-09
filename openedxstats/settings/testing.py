4ab1750b22c1876ae4bf9432510cf7f8b594c74e
Ned Batchelder <ned@nedbatchelder.com>
2016-08-09 17:04:19 -0400

    Test settings use SQLite

--- openedxstats/settings/testing.py
+++ openedxstats/settings/testing.py
@@ -1,1 +1,12 @@
 from .base import *  # noqa
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',  # Set to empty string for default.
+    }
+}
