751dada1c7335f8bc5b485500fc1147c7bd1a643
Zachary Robbins <zrrobbins@wpi.edu>
2016-08-09 11:26:01 -0400

    Drew out JS ajax preparation into separate JS file. Added redirect from home page to sites list until home page is added.

---
+++ openedxstats/apps/sites/static/sites/prepareAjax.js
@@ -0,0 +1,41 @@
+/*
+ * Include this file when using AJAX and/or JS POST requests, and call getCSRFToken to acquire the Django CSRF
+ * token. It is required to pass the CSRF token in the request to prevent denial by Django.
+ */
+
+// Grab cookie
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+var csrftoken = getCookie('csrftoken');
+
+// Set ajax requests to use csrf token
+$.ajaxSetup({
+    beforeSend: function (xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
+function getCSRFToken() {
+    return csrftoken;
+}
--- openedxstats/views.py
+++ openedxstats/views.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.core.files.storage import default_storage
 
 from django.views.generic.base import TemplateView
-from django.contrib import messages
-
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
         context = super(HomePageView, self).get_context_data(**kwargs)
-        messages.info(self.request, 'hello http://example.com')
         return context
+
+    # TODO: Remove this method once a home page is created
+    def dispatch(self, request, *args, **kwargs):
+        # Redirect to sites list
+        return HttpResponseRedirect(reverse('sites:sites_list'))
