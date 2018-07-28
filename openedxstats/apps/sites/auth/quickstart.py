f1d9ba065af98198a7edfa09bf1c3c1014bee794
Bryan Le <bryan@bryanle.io>
2018-07-28 15:10:14 -0400

    Replaced origin of Google sheets from OAuth flow to csv download

---
+++ openedxstats/apps/sites/auth/quickstart.py
@@ -0,0 +1,30 @@
+"""
+Shows basic usage of the Sheets API. Prints values from a Google Spreadsheet.
+"""
+from __future__ import print_function
+from apiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+# Setup the Sheets API
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+store = file.Storage('token.json')
+creds = None
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+service = build('sheets', 'v4', http=creds.authorize(Http()))
+
+# Call the Sheets API
+SPREADSHEET_ID = '1v5EWlgFpXtDKORXcm8OWSgFsUHYuCR2ick5Q4kdnrQk'
+RANGE_NAME = 'A:H'
+result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,
+                                             range=RANGE_NAME).execute()
+values = result.get('values', [])
+if not values:
+    print('No data found.')
+else:
+    print('Name, Major:')
+    for row in values:
+        # Print columns A and E, which correspond to indices 0 and 4.
+        print('%s, %s' % (row[0], row[4]))
