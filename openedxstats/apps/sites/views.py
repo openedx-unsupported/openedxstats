216aa16b823b7c3430758f482a0462f5ef080cba
Bryan Le <bryan@bryanle.io>
2018-08-19 13:35:32 -0400

    Adding additional exclusions

--- openedxstats/apps/sites/views.py
+++ openedxstats/apps/sites/views.py
@@ -81,7 +81,7 @@ class HawthornMapView(generic.ListView):
     # context_object_name = 'sites_map'
 
 def stats_view(request):
-    active_sites_count = Site.objects.exclude(active_end_date__isnull=False).count()
+    active_sites_count = Site.objects.exclude(active_end_date__isnull=False).filter(course_count == 0).count()
     active_sites_course_count = Site.objects.exclude(active_end_date__isnull=False).aggregate(Sum('course_count'))['course_count__sum']
     language_count = Language.objects.all().count()
     geozones_count = GeoZone.objects.all().count()
@@ -198,7 +198,7 @@ class OTChartView(generic.list.MultipleObjectTemplateResponseMixin, generic.list
                 Q(is_gone=False) &
                 date_select
             ).aggregate(sites=Count('*'), courses=Sum('course_count'))
-
+            print('day_Stats', day_stats)
             try:
                 over_count = OverCount.objects.get(date_select).course_count
             except OverCount.DoesNotExist:
