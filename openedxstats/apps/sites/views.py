9ee0463d674ede1498f79c6fb88c21360c4269c5
Bryan Le <bryan@bryanle.io>
2018-08-20 17:52:20 -0400

    Revised function name and docstring

--- openedxstats/apps/sites/views.py
+++ openedxstats/apps/sites/views.py
@@ -80,7 +80,7 @@ class HawthornMapView(generic.ListView):
     template_name = 'sites/hawthorn_map.html'
 
 def stats_view(request):
-    active_sites = Site.objects.exclude(active_end_date__isnull=False).filter(get_valid_courses()).aggregate(sites=Count('*'), courses=Sum('course_count'))
+    active_sites = Site.objects.exclude(active_end_date__isnull=False).filter(valid_course_query()).aggregate(sites=Count('*'), courses=Sum('course_count'))
     over_count = OverCount.objects.all().last().course_count
     valid_course_count = active_sites['courses'] - over_count
     language_count = Language.objects.all().count()
@@ -195,7 +195,7 @@ class OTChartView(generic.list.MultipleObjectTemplateResponseMixin, generic.list
             )
 
             day_stats = Site.objects.filter(
-                get_valid_courses() &
+                valid_course_query() &
                 date_select
             ).aggregate(sites=Count('*'), courses=Sum('course_count'))
             try:
@@ -454,9 +454,9 @@ def bulk_update(request):
 
     return json_response(data=resp)
 
-def get_valid_courses():
+def valid_course_query():
     """
-    Helper function for stats_view and OTChartView to retrieve valid courses
+    Helper function for stats_view and OTChartView to query valid courses
     """
     return (Q(course_count__gt=0) | Q(is_private_instance=True)) & Q(is_gone=False)
 
