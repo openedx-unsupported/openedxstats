ac2df989be776033b66cbd30072f817f4f3da8f6
bryankle <bryan@bryanle.io>
2018-08-21 16:44:52 -0400

    Course count on countries match table course count (#50)
    * Course count on cluster matches course count on table.

--- openedxstats/apps/sites/views.py
+++ openedxstats/apps/sites/views.py
@@ -24,7 +24,7 @@ from openedxstats.apps.sites.forms import SiteForm, LanguageForm, GeoZoneForm
 
 # Converts site data into JSON format for Ajax request
 def SiteView_JSON(request):
-    sites = Site.objects.all()
+    all_sites = Site.objects.all()
     active_sites = Site.objects.exclude(active_end_date__isnull=False)
     active_sites_id_set = set(map(lambda site:site['id'], active_sites.values('id')))
     country_list = list(map(lambda country:country['geo_zone'], SiteGeoZone.objects.values('geo_zone').order_by('geo_zone').distinct()))
@@ -33,13 +33,13 @@ def SiteView_JSON(request):
     for loc in all_geozone:
         site_id = loc['site_id']
         geo_zone = loc['geo_zone']
-        if site_id in active_sites_id_set:
+        if site_id in active_sites_id_set and active_sites.get(id=site_id).course_count != 0:
             country_site_count_dict[geo_zone] += 1
     geo = SiteGeoZone.objects.all()
     geo_json = serializers.serialize("json", geo)
     language = SiteLanguage.objects.all()
     language_json = serializers.serialize("json", language)
-    sites_json = serializers.serialize("json", sites)
+    sites_json = serializers.serialize("json", all_sites)
     return JsonResponse({'sites': sites_json, 'geo': geo_json, 'language': language_json, 'activeSitesCount': country_site_count_dict})
 
 # Downloads public Google Sheets for Hawthorn user data as CSV and parses into JSON
