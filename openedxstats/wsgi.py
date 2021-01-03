b960c7ecfb009540c2565081b9d117c993df83f0
Ned Batchelder <ned@edx.org>
2021-01-03 09:03:55 -0500

    Adapt to whitenoise v4
    http://whitenoise.evans.io/en/stable/changelog.html#v4-0

--- openedxstats/wsgi.py
+++ openedxstats/wsgi.py
@@ -20,7 +20,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openedxstats.settings")
 
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
