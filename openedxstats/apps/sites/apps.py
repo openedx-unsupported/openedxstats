4bffd8aa19e578532fc6553764a2d1a66f5da417
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-07-23 15:32:39 +0500

    refactor: Removed the django32 condition.

--- openedxstats/apps/sites/apps.py
+++ openedxstats/apps/sites/apps.py
@@ -1,8 +1,6 @@
-import django
 from django.apps import AppConfig
 
 
 class SitesConfig(AppConfig):
     name = 'sites'
-    if django.VERSION >= (3, 2):
-        default = False
+    default = False
--- openedxstats/apps/slackdata/apps.py
+++ openedxstats/apps/slackdata/apps.py
@@ -1,8 +1,6 @@
-import django
 from django.apps import AppConfig
 
 
 class SlackdataConfig(AppConfig):
     name = 'slackdata'
-    if django.VERSION >= (3, 2):
-        default = False
+    default = False
