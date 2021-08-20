fb44b17c9c6a5ae644c63d90b3a24b4fdd85cdf9
Shimul Chowdhury <shimul@opencraft.com>
2021-08-20 11:51:15 +0800

    fix: replace django-dotenv with django-environ-2

--- manage.py
+++ manage.py
@@ -2,13 +2,13 @@
 import os
 import sys
 
-from dotenv import load_dotenv
+import environ
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openedxstats.settings")
 
-    dotenv_path = os.path.join(os.path.dirname(__file__), 'openedxstats/.env')
-    load_dotenv(dotenv_path)
+    env_file_path = os.path.join(os.path.dirname(__file__), 'openedxstats/.env')
+    environ.Env.read_env(env_file_path)
 
     from django.core.management import execute_from_command_line
 
